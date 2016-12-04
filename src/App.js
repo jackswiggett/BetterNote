e75d3888c6512e27779f9d7b21de3d0e28db6d6e
Jack Swiggett <jackswiggett@gmail.com>
2016-12-03 17:57:02 -0800

    Add React components for notation, keyboard, and next button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
+const notesSequence = [
+  ["F"],
+  ["G, B, D"],
+  ["F#"]
+]
+
+function Notation(props) {
+  return (
+    <div className="notation">
+      <p>{props.notes}</p>
+    </div>
+  );
+}
+
+function Keyboard(props) {
+  return (
+    <div className="keyboard">
+      <p>We will render the keyboard here.</p>
+    </div>
+  );
+}
+
+function NextButton(props) {
+  return (
+    <button
+      className="next-button"
+      onClick={props.onClick}>
+      Next
+    </button>
+  );
+}
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentNotesIndex: 0
+    };
+  }
+
+  nextNotes() {
+    const index = this.state.currentNotesIndex;
+    if (index < notesSequence.length - 1) {
+      this.setState({currentNotesIndex: index + 1})
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Notation Test</h1>
+        <Notation notes={notesSequence[this.state.currentNotesIndex]}/>
+        <Keyboard />
+        <NextButton onClick={() => this.nextNotes()} />
       </div>
     );
   }
